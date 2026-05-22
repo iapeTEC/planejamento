6d1bec4d3a8bac15e4ea689e6d488d14fc91f415
IAPE <bruno.souza4@eaportal.org>
2026-05-22 15:56:37 -0300

    Add files via upload

--- platform-config.js
+++ platform-config.js
@@ -1,6 +1,6 @@
 window.LESSON_PREP_CONFIG = {
   gasUrl: "https://script.google.com/macros/s/AKfycbw6vlkhv8Di8aU1C02WXbymEi7gJCgVGhwEpCSBUWOzfsRhWYDN7RuYCYKcUF0YZI969g/exec",
-  googleClientId: "bruno.souza4@eaportal.org",
+  googleClientId: "433057640119-o2trlpqs7lac8kt2lbseitnm372em89b.apps.googleusercontent.com",
   adminEmails: [
     "normafederal@gmail.com"
   ],
