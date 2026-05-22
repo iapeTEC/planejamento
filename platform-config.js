bfd173b3e02023c396ec4b7c375f0697b090bbe5
IAPE <bruno.souza4@eaportal.org>
2026-05-22 15:43:15 -0300

    Update googleClientId in platform-config.js

--- platform-config.js
+++ platform-config.js
@@ -1,6 +1,6 @@
 window.LESSON_PREP_CONFIG = {
   gasUrl: "https://script.google.com/macros/s/AKfycbw6vlkhv8Di8aU1C02WXbymEi7gJCgVGhwEpCSBUWOzfsRhWYDN7RuYCYKcUF0YZI969g/exec",
-  googleClientId: "",
+  googleClientId: "bruno.souza4@eaportal.org",
   adminEmails: [
     "normafederal@gmail.com"
   ],
