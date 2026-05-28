ca6def3cb7cf1cdb1eef567605ba1212c0efe0e9
IAPE <bruno.souza4@eaportal.org>
2026-05-28 09:49:18 -0300

    Update platform-config.js

--- platform-config.js
+++ platform-config.js
@@ -1,5 +1,5 @@
 window.LESSON_PREP_CONFIG = {
-  gasUrl: "https://script.google.com/macros/s/AKfycbzi1Di4JCDM3NdbeHFgijWptqvwjqQSHhRraGSwyC7aptQsigR2cAjR4D8U0ejfOoNcbA/exec",
+  gasUrl: "https://script.google.com/macros/s/AKfycbzAYRQ5NSe5rtRYMDUl6pAD5qNufDCItCikd8p3nUhXYPnI9mdI6v-5AVo67mb3RW-W7A/exec",
   googleClientId: "433057640119-o2trlpqs7lac8kt2lbseitnm372em89b.apps.googleusercontent.com",
   adminEmails: [
     "normafederal@gmail.com"
